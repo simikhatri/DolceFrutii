f3aabadbe406d1b540c7422349dbe6c401aecdc4
simikhatri <simrankkhatri1@gmail.com>
2024-02-12 06:18:44 -0800

    images

--- src/components/shop/Slider.js
+++ src/components/shop/Slider.js
@@ -1,5 +1,8 @@
 import React from "react";
 import "bootstrap/dist/js/bootstrap.bundle.min.js";
+import pic1 from "../../images/pic1.jpg"
+import pic2 from "../../images/pic2.jpg"
+import pic3 from "../../images/pic3.jpg"
 import "./css/slider.css";
 
 const Slider = () => {
@@ -12,20 +15,19 @@ const Slider = () => {
       >
         <div className="carousel-inner">
           <div className="carousel-item active" data-bs-interval="3000">
-            <div className="img1 img"></div>
+            {/* <div className="img1 img"></div> */}
+            <img src={pic1} alt="" className="img" />
           </div>
           <div className="carousel-item" data-bs-interval="3000">
-            <div className="img2 img"></div>
+            <img src={pic2} alt="" className="img" />
           </div>
           <div className="carousel-item" data-bs-interval="3000">
-            <div className="img3 img"></div>
+            <img src={pic3} alt="" className="img" />
           </div>
         </div>
         <div className="carousel-caption">
           <h1 className="mb-2">Fresh, Healthy & Organic</h1>
           <h4 className="mb-3">100% organic Fruits</h4>
-          {/* <h4 className="mb-3">100% organic Fruits</h4>
-          <h4 className="mb-3">100% organic Fruits</h4> */}
           <div className="btn-shop">
             <button>Shop</button>
           </div>
