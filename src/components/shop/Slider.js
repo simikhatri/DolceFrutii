acb9eb4a8583a75f80e65a1481354c3fdbca6325
simikhatri <simrankkhatri1@gmail.com>
2024-02-11 22:00:13 -0800

    changes commit

--- src/components/shop/Slider.js
+++ src/components/shop/Slider.js
@@ -16,24 +16,16 @@ const Slider = () => {
           </div>
           <div className="carousel-item" data-bs-interval="3000">
             <div className="img2 img"></div>
-            {/* <div className="carousel-caption">
-                            <h2 className='mb-2 carouselhead'>7+ varieties of Organic Dates</h2>
-                            <button>BUY SOME</button>
-                        </div> */}
           </div>
           <div className="carousel-item" data-bs-interval="3000">
             <div className="img3 img"></div>
-            {/* <div className="carousel-caption thirdSlide">
-                            <h2 className='mb-2'>Grow in the USA, the Organic way</h2>
-                            <button>Learn More</button>
-                        </div> */}
           </div>
         </div>
         <div className="carousel-caption">
           <h1 className="mb-2">Fresh, Healthy & Organic</h1>
           <h4 className="mb-3">100% organic Fruits</h4>
-          <h4 className="mb-3">100% organic Fruits</h4>
-          <h4 className="mb-3">100% organic Fruits</h4>
+          {/* <h4 className="mb-3">100% organic Fruits</h4>
+          <h4 className="mb-3">100% organic Fruits</h4> */}
           <div className="btn-shop">
             <button>Shop</button>
           </div>
