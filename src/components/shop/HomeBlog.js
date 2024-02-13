89046a05fce631c38c49f17af0c7195cd9722dfd
simikhatri <simrankkhatri1@gmail.com>
2024-02-13 04:08:15 -0800

    last commit

--- src/components/shop/HomeBlog.js
+++ src/components/shop/HomeBlog.js
@@ -1,9 +1,6 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import './css/Shop.css'
-import 'swiper/css';
-import 'swiper/css/grid';
-import 'swiper/css/navigation';
 import dates from '../../Assets/images/cm9.jpg'
 import pome from '../../Assets/images/pomog.jpg'
 import fig from '../../Assets/images/fig.jpg'
@@ -62,85 +59,6 @@ const HomeBlog = () => {
                             <Link to="/blog" className='btn'>More Blogs</Link>
                         </div>
                     </div>
-                    {/* <Swiper className='Swiper'
-                        slidesPerView={3}
-                        breakpoints={{
-                            900: { slidesPerView: 3 },
-                            600: { slidesPerView: 2 },
-                            450: { slidesPerView: 1 },
-                            270: { slidesPerView: 1 },
-                        }}
-                        navigation={true}
-                        modules={[Navigation]}
-                        spaceBetween={1}>
-                        <SwiperSlide>
-                            <div className="slide">
-                                <div className="card" >
-                                    <img src={dates} className="card-img-top img-fluid" alt="..." />
-                                    <div className="card-body text-start">
-                                        <h5 className="card-title">Dates</h5>
-                                        <p>Nature's sweet gems, a delightful blend of chewy and rich goodness.</p>
-                                        <Link to="/blog" className="shop">Shop Now
-                                            <MdNavigateNext /></Link>
-                                    </div>
-                                </div>
-                            </div>
-                        </SwiperSlide>
-                        <SwiperSlide>
-                            <div className="slide">
-                                <div className="card" >
-                                    <img src={pome} className="card-img-top" alt="..." />
-                                    <div className="card-body text-start">
-                                        <h5 className="card-title">Pomegranates</h5>
-                                        <p>Bursting jewels, a taste explosion of sweet and tart perfection.</p>
-                                        <Link to="/blog" className="shop">Shop Now
-                                            <MdNavigateNext /></Link>
-                                    </div>
-                                </div>
-                            </div>
-                        </SwiperSlide>
-                        <SwiperSlide>
-                            <div className="slide">
-                                <div className="card" >
-                                    <img src={fig} className="card-img-top" alt="..." />
-                                    <div className="card-body text-start">
-                                        <h5 className="card-title">Figs</h5>
-                                        <p> Nature's candy, a sweet and chewy delight for a wholesome treat.</p>
-                                        <Link to="/blog" className="shop">Shop Now
-                                            <MdNavigateNext /></Link>
-                                    </div>
-                                </div>
-                            </div>
-                        </SwiperSlide>
-                        <SwiperSlide>
-                            <div className="slide">
-                                <div className="card" >
-                                    <img src={orange} className="card-img-top" alt="..." />
-                                    <div className="card-body text-start">
-                                        <h5 className="card-title">Citrus</h5>
-                                        <p> Tangy joy, a burst of freshness in every zesty citrus moment.</p>
-                                        <Link to="/blog" className="shop">Shop Now
-                                            <MdNavigateNext /></Link>
-                                    </div>
-                                </div>
-                            </div>
-                        </SwiperSlide>
-                        <SwiperSlide>
-                            <div className="slide">
-                                <div className="card" >
-                                    <img src={mango} className="card-img-top" alt="..." />
-                                    <div className="card-body text-start">
-                                        <h5 className="card-title">Mangos</h5>
-                                        <p>
-                                            Tropical bliss, a juicy symphony of sweetness in every bite.
-                                        </p>
-                                        <Link to="/blog" className="shop">Shop Now
-                                            <MdNavigateNext /></Link>
-                                    </div>
-                                </div>
-                            </div>
-                        </SwiperSlide>
-                    </Swiper> */}
                 </div>
             </div >
         </>
--- src/components/shop/Slider.js
+++ src/components/shop/Slider.js
@@ -34,25 +34,6 @@ const Slider = () => {
       </div>
     </div>
 
-    // <Swiper
-    //   slidesPerView={1}
-    //   modules={[Navigation]}
-    //   navigation={true}
-    //   className="swiper">
-    //   <SwiperSlide>
-    //     <img src="https://swiperjs.com/demos/images/nature-1.jpg" />
-    //   </SwiperSlide>
-    //   <SwiperSlide>
-    //     <img src="https://swiperjs.com/demos/images/nature-2.jpg" />
-    //   </SwiperSlide>
-    //   <SwiperSlide>
-    //     <img src="https://swiperjs.com/demos/images/nature-3.jpg" />
-    //   </SwiperSlide>
-    //   <SwiperSlide>
-    //     <img src="https://swiperjs.com/demos/images/nature-4.jpg" />
-    //   </SwiperSlide>
-    // </Swiper> 
-
   );
 };
 
