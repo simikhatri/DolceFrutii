59dd09868100d31fbc2db8e9d6e7881234d4b812
simikhatri <simrankkhatri1@gmail.com>
2024-03-05 20:43:14 -0800

    new changes

--- src/components/dateDescription/index.js
+++ src/components/dateDescription/index.js
@@ -47,8 +47,8 @@ const index = () => {
                                     experience rooted in the ethos of organic farming and environmental stewardship.
                                 </p>
                             </div>
-                            <div className='text-start mt-4 pt-2'>
-                                <h2>What Makes Barhi Dates Special?</h2>
+                            <div className='my-4 py-3'>
+                                <h2 className='mb-3'>What Makes Barhi Dates Special?</h2>
                                 <p> Barhi Dates stand out for their unique creamy texture and intense
                                     sweetness, resembling honey and caramel flavors. They're not just
                                     a treat for the palate but also a nutritional powerhouse, offering
@@ -59,7 +59,7 @@ const index = () => {
                                     variety in the culinary and health food sectors.
                                 </p>
                             </div>
-                            <div className='text-start mt-4 pt-2'>
+                            {/* <div className='text-start mt-4 pt-2'>
                                 <h2 className='mb-4'>The Journey of Barhi Dates</h2>
                                 <h3>From Orchard to Table:</h3>
                                 <ul>
@@ -68,8 +68,8 @@ const index = () => {
                                     <li>Harvested at peak sweetness, carefully sorted, and packaged.</li>
                                     <li>Transported under optimal conditions to maintain freshness and quality</li>
                                 </ul>
-                            </div>
-                            <div className='text-start mt-4 pt-2'>
+                            </div> */}
+                            {/* <div className='text-start mt-4 pt-2'>
                                 <h3>Organic Farming Practices:</h3>
                                 <ul>
                                     <li> Avoids synthetic pesticides and fertilizers, utilizing natural alternatives.</li>
@@ -77,16 +77,24 @@ const index = () => {
                                     <li>Promotes soil health and biodiversity, ensuring sustainable cultivation</li>
                                     <li>Results in superior taste, nutritional value, and environmental benefits.</li>
                                 </ul>
-                            </div>
-                            <div className='text-start mt-4 pt-2'>
+                            </div> */}
+                            {/* <div className='text-start mt-4 pt-2'>
                                 <h3>California's Role in Barhi Dates Production</h3>
                                 <p>California plays a pivotal role in the cultivation of Barhi dates, a fruit cherished for its unique sweetness and texture. This significance is deeply rooted in the state's history of date farming, particularly in the Coachella Valley, which stands out as an ideal location for producing these high-quality dates</p>
+                            </div> */}
+                            <div className='my-4 py-3'>
+                                <h2 className='mb-3'>History of Dates Farming in California</h2>
+                                <p>The journey of date farming in California began in the early 20th
+                                    century when the U.S. Department of Agriculture introduced date palms
+                                    to the region as part of an agricultural experiment. The goal was to
+                                    find crops that would thrive in the desert-like conditions of California's
+                                    interior. The introduction of date palms, including the Barhi variety,
+                                    proved successful, marking the beginning of what would become a
+                                    flourishing industry. Over the years, farmers in California honed
+                                    their skills and knowledge in date cultivation, adopting techniques
+                                    that would enhance the quality and yield of their crops.</p>
                             </div>
-                            <div className='text-start mt-4 pt-2'>
-                                <h3>History of Dates Farming in California</h3>
-                                <p>The journey of date farming in California began in the early 20th century when the U.S. Department of Agriculture introduced date palms to the region as part of an agricultural experiment. The goal was to find crops that would thrive in the desert-like conditions of California's interior. The introduction of date palms, including the Barhi variety, proved successful, marking the beginning of what would become a flourishing industry. Over the years, farmers in California honed their skills and knowledge in date cultivation, adopting techniques that would enhance the quality and yield of their crops.</p>
-                            </div>
-                            <div className='text-start mt-4 pt-2'>
+                            {/* <div className='text-start mt-4 pt-2'>
                                 <h3>The Coachella Valley</h3>
                                 <ul>
                                     <li> The Coachella Valley's hot, dry climate and ample sunshine closely
@@ -100,8 +108,8 @@ const index = () => {
                                     <li>California's efforts in producing Barhi dates have not only satisfied the American market but
                                         also positioned it as a leader in environmentally responsible date farming globally.</li>
                                 </ul>
-                            </div>
-                            <div className='text-start mt-4 pt-2'>
+                            </div> */}
+                            {/* <div className='text-start mt-4 pt-2'>
                                 <h3>Revolutionary Farming Methods for Organic Barhi Dates</h3>
                                 <ul>
                                     <li>Farmers use natural stuff like compost to make the soil good for growing organic Barhi dates, without any fake chemicals.</li>
@@ -111,8 +119,8 @@ const index = () => {
                                     <li>When the dates are just perfect, they pick them by hand to make sure they're sweet and good for you, without hurting them.</li>
                                     <li>After picking, they let the dates dry in the sun, which keeps them tasty and makes them last longer without using anything artificial.</li>
                                 </ul>
-                            </div>
-                            <div className='text-start mt-4 pt-2'>
+                            </div> */}
+                            {/* <div className='text-start mt-4 pt-2'>
                                 <h3>Harvest and Post-Harvest Techniques</h3>
                                 <p>Harvesting organic Barhi dates is all about timing and care.
                                     Farmers handpick each date at just the right moment to keep them sweet and undamaged.
@@ -125,9 +133,9 @@ const index = () => {
                                     keeping them tasty and healthy. Through these steps, the dates not only stick to organic
                                     standards but also end up being a real treat for anyone who tries them.
                                 </p>
-                            </div>
-                            <div className='text-start mt-4 pt-2'>
-                                <h3>Benefits of Eating Organic Barhi Dates</h3>
+                            </div> */}
+                            <div className='my-4 pt-3'>
+                                <h2 className='mb-3'>Benefits of Eating Organic Barhi Dates</h2>
                                 <p>Eating organic Barhi dates offers numerous benefits, blending delicious sweetness with powerful nutrition. These natural gems are packed with fiber, helping improve digestion, and are a great source of energy-boosting natural sugars. They also come loaded with essential minerals like potassium, magnesium, and iron, supporting heart health and overall well-being. Opting for organic means you're choosing dates grown without synthetic pesticides or fertilizers, making them a healthier choice for you and better for the planet. Whether enjoyed as a snack or used to sweeten recipes, organic Barhi dates are a wholesome addition to any diet.
                                 </p>
                             </div>
